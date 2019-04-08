c77d2f6f40885bf45899c558a7bb0bafe0131688
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-04-08 00:26:38 -0700

    maintaining extension metadata and hotword dict

--- background.js
+++ background.js
@@ -5,11 +5,33 @@ function log(message){
   console.log("-----------------------");
 }
 
-function onMessageRecieved(message, sender){
-  log("INNNCOMMMINGGGG");
-  log(message);
-  log(sender);
+//this is terrible code, but it works
+function updateBeevesMetadata(message, sender){
+  browser.storage.local.get('beeves_metadata', function(beeves_metadata){
+    beeves_metadata = beeves_metadata.beeves_metadata || beeves_metadata;
+    beeves_metadata[sender.id] = message;
+    browser.storage.local.set({beeves_metadata}, function(){});
+  });
+  browser.storage.local.get('beeves_hotwords', function(beeves_hotwords){
+    beeves_hotwords = beeves_hotwords.beeves_hotwords || beeves_hotwords;
+    beeves_hotwords[message.beeves.hotword] = sender.id;
+    browser.storage.local.set({beeves_hotwords}, function(){printStorage();});
+  });
 }
+
+//for testing only
+function printStorage(){
+  browser.storage.local.get(['beeves_metadata', 'beeves_hotwords'], function(data){
+    log(data);
+  });
+}
+
+function onMessageRecieved(message, sender){
+  //trainNLUBackend
+  updateBeevesMetadata(message, sender);
+  //flash 'new skill added' on agent
+}
+
 browser.runtime.onMessageExternal.addListener(onMessageRecieved);
 
 browser.browserAction.onClicked.addListener(function(){
@@ -22,3 +44,4 @@ browser.browserAction.onClicked.addListener(function(){
   var creating = browser.windows.create(createData);
 });
 
+var clearStorage = browser.storage.local.clear();
