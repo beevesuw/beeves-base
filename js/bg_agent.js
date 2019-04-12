f00b95e571702292cc6fc21674e2b084080cb7e4
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-04-12 14:49:46 -0700

    about to start messaging from stt

--- js/bg_agent.js
+++ js/bg_agent.js
@@ -1,14 +1,15 @@
-// browser.browserAction.onClicked.addListener(function(){
-//     var createData = {
-//       type: "detached_panel",
-//       url: "agent.html",
-//       width: 250,
-//       height: 250
-//     };
-//     var let = browser.windows.create(createData);
-//   });
+browser.browserAction.onClicked.addListener(function(){
+    var createData = {
+      type: "detached_panel",
+      url: "agent.html",
+      width: 250,
+      height: 250
+    };
+    var let = browser.windows.create(createData);
+  });
 
-browser.browserAction.onClicked.addListener(async function(){
-  let result = await new BeevesRPC('extension@beeves.com', 'test', {'data':'data'}).execute();
-});
+// browser.browserAction.onClicked.addListener(async function(){
+//   await new BeevesRPC('extension@beeves.com', 'add', {'n1':2, 'n2':3}).execute();
+//   await new BeevesRPC('extension@beeves.com', 'subtract', {'n1':4, 'n2':2}).execute();
+// });
 
