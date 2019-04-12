1694c07d3deb40889f3521424cba6c153978270e
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-04-12 15:48:10 -0700

    updates

--- js/bg_dispatcher.js
+++ js/bg_dispatcher.js
@@ -1,6 +1,7 @@
 browser.runtime.onMessage.addListener(dispatch);
 
-async function dispatch(text) {
-  console.log(text);
+async function dispatch(message) {
+  console.log(message['text'].split(' '));
+  if(message['text'].split(' ')[0]=='fail')return Promise.resolve('FAILURE');
   return Promise.resolve('success!')
 }
