1c6ea3d0f5fa90c197a93be7b076ae4f78f2806c
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-04-12 18:59:40 -0700

    stable, added post util

--- js/bg_utils.js
+++ js/bg_utils.js
@@ -8,3 +8,21 @@ function log(message){
     console.log(message);
     console.log("-----------------------");
   }
+
+  async function postData(endpoint, payload) {
+    try {
+      let res = await fetch(endpoint, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      });
+      res = await res.json();
+      //log(res);
+      return res;
+    }catch(err) {
+      console.log(err);
+    }
+  }
